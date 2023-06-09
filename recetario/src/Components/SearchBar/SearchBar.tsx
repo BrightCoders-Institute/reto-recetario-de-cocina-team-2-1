d7c2cf3539fcb03eef20baae448d13efbffcc89e
ruruisu <zaddozgl@gmail.com>
2023-06-09 17:49:35 -0600

    added styles to the searchbar

--- recetario/src/Components/SearchBar/SearchBar.tsx
+++ recetario/src/Components/SearchBar/SearchBar.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet, Dimensions } from 'react-native';
 import { Icon } from 'react-native-elements';
+
+const { width } = Dimensions.get('window');
 
 const SearchBar: React.FC = () => {
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-      <TextInput
-        style={{ flex: 1, height: 40, borderColor: 'gray', borderWidth: 1, paddingHorizontal: 10 }}
-        placeholder="Search..."
-      />
-      <Icon name="search" type="material" />
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search..."
+        />
+        <Icon name="search" type="material" style={styles.icon} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    width: width * 0.8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    paddingHorizontal: 10,
+    fontSize: 16,
+  },
+  icon: {
+    marginLeft: 10,
+  },
+});
 
 export default SearchBar;
