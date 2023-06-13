5a7e4bc29ab61221dec8cef899cc50807a150c2e
CUBOFIG <figmiche@gmail.com>
2023-06-13 17:46:29 -0600

    feature/recipe-view

--- recetario/App.tsx
+++ recetario/App.tsx
@@ -1,12 +1,91 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import SearchBar from './src/Components/SearchBar/SearchBar'; 
-const App: React.FC = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <SearchBar/>
-    </View>
-  );
+import React, {useState} from 'react';
+import {View, Button, StyleSheet} from 'react-native';
+import {RecipeView, SearchBar} from './src/components';
+
+const demo = {
+  id: 1,
+  image:
+    'https://images-gmi-pmc.edge-generalmills.com/7bb42593-aca1-4740-9a74-0d60abc658f8.jpg',
+  categoria: 'trending',
+  name: 'Tacos de pollo al chipotle',
+  ingredients: [
+    {
+      name: 'pollo',
+      amount: '200 gramos',
+    },
+    {
+      name: 'tortillas de maíz',
+      amount: '8 unidades',
+    },
+    {
+      name: 'salsa de chipotle',
+      amount: '2 cucharadas',
+    },
+    {
+      name: 'cebolla',
+      amount: '1/2 unidad',
+    },
+    {
+      name: 'cilantro',
+      amount: 'al gusto',
+    },
+    {
+      name: 'limón',
+      amount: '2 unidades',
+    },
+    {
+      name: 'salsa de chipotle',
+      amount: '2 cucharadas',
+    },
+    {
+      name: 'cebolla',
+      amount: '1/2 unidad',
+    },
+    {
+      name: 'cilantro',
+      amount: 'al gusto',
+    },
+    {
+      name: 'limón',
+      amount: '2 unidades',
+    },
+  ],
+  amount: 4,
 };
 
+function App(): JSX.Element {
+  const [recipe, setRecipe] = useState({});
+  const [showView, setShowView] = useState(false);
+
+  const showElement = () => {
+    setRecipe(demo);
+    setShowView(true);
+  };
+
+  return (
+    <View style={styles.recipeView}>
+      <Button title="Press me" onPress={() => showElement()} />
+
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <SearchBar />
+      </View>
+
+      {showView && (
+        <RecipeView
+          recipe={recipe}
+          closeView={() => setShowView(prevState => !prevState)}
+        />
+      )}
+    </View>
+  );
+}
+
 export default App;
+
+const styles = StyleSheet.create({
+  recipeView: {
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+  },
+});
